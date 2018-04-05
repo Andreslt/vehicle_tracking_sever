2f3eaa1aef27275d5f0ad3ac8fa9b3b4a84bc9bf
Andres Lastra <Andres@MacBook-Pro-de-Andres-2.local>
2018-04-05 01:41:40 -0500

    adding text

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const bodyParser = require('body-parser')
 app.use(require('cors')());
 app.use(bodyParser.json());
 app.use('/api', api);
-
+app.get('/', (req, res) => {
+  res.send('Welcome to the Smart-Tracking System API!')
+})
 app.set('port', (process.env.PORT || 8080));
 
 app.listen(app.get('port'), function () {
