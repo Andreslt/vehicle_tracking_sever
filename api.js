83a58f429fff2687450a5807dc0bcaae78f0fc3f
Andres Lastra <Andres@MacBook-Pro-de-Andres-2.local>
2018-04-26 07:45:35 -0500

    fixing csv export delay

--- api.js
+++ api.js
@@ -5,6 +5,7 @@ const json2csv = require('json2csv').parse;
 const firebase = require("firebase-admin");
 const serviceAccount = require('./firebase.json');
 const fs = require('fs');
+const moment = require('moment');
 
 const dbURL = "https://ss-smtracking.firebaseio.com"
 firebase.initializeApp({
@@ -41,15 +42,23 @@ router.post('/savetrail', (req, res) => {
 })
 
 router.post('/downloadcsv', (req, res) => {
-  const data = []; // data.push(snap.val()[key] )
+  const time1 = moment(new Date());
+  // console.log('*** time 1 -> ', time1.format('DD/MM HH:mm:ss'));
   fB.child('trails').orderByChild('sent_tsmp').startAt(req.body.startingDate).endAt(req.body.endingDate)
     .on('value', snap => {
       const snapVal = snap.val();
+      const time2 = moment(new Date());
+      // console.log('*** time 2 -> ', time2.format('DD/MM HH:mm:ss'), '.:. Diff ->', moment(time2.diff(time1)).format("m[m] s[s]"));
       if (!snapVal) return res.status(400).send({ message: 'Los resultados no contienen registros. Modifique los parámetros de búsqueda.' });
-      Object.keys(snapVal).map(key => data.push(snap.val()[key]));
+      const data = Object.values(snap.val())
       const fields = ['id', 'altitude', 'latitude', 'longitude', 'sent_tsmp', 'speed', 'track', 'vehicle_id', 'zone_vehicle'];
       const opts = { fields };
+      const time3 = moment(new Date());
+      // console.log('*** time 3 -> ', time3.format('DD/MM HH:mm:ss'), '.:. Diff ->', moment(time3.diff(time2)).format("m[m] s[s]"));
       const csv = json2csv(data, opts);
+      const time4 = moment(new Date());
+      // console.log('*** time 4 -> ', time3.format('DD/MM HH:mm:ss'), '.:. Diff ->', moment(time4.diff(time3)).format("m[m] s[s]"));
+      
       return res.status(200).send(csv);
     })
 })
