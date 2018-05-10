da5d60dabdd0d1c487dd4b4577c3ba115d9d9011
Andres Lastra <Andres@MacBook-Pro-de-Andres-3.local>
2018-05-10 14:53:04 -0500

    implementing dbRefactoring

--- api.js
+++ api.js
@@ -15,24 +15,24 @@ firebase.initializeApp({
 
 const fB = firebase.database().refFromURL(dbURL);
 
-exports.getZoneOfVehicle = (vehicle_id, done) => {
-  fB.child('vehicles').on('value', snap => {
-    const vehicles = snap.val();
-    let vehArray = [], vehiclePicked;
-    Object.keys(vehicles).map(key => {
-      vehArray.push(vehicles[key]);
-    })
-    vehiclePicked = vehArray.filter(key => { return key.id === vehicle_id });
-    if (!!vehiclePicked) done(vehiclePicked[0])
+exports.getCompanyOfVehicle = (vehicle_id, done) => {
+  fB.child('CONTROL/VEHICLES').orderByChild('id').equalTo(vehicle_id).on('value', snap => {
+    done(snap.val());
   })
 }
 
 router.post('/savetrail', (req, res) => {
-  exports.getZoneOfVehicle(req.body.vehicle_id, cb => {
+  let data = req.body;
+  exports.getCompanyOfVehicle(data.vehicle_id, cb => {
     if (!cb) return res.status(404).send('Vehicle not found.')
-    const data = req.body;
-    data.zone_vehicle = `${cb.zone_id}_${req.body.vehicle_id}`;
-    fB.child('trails').push().set(data).then(function (result) {
+    const vh = cb[Object.keys(cb)[0]];
+    const company = vh.zone.slice(0, 5);
+    const path = `DATA/ENTITIES/${company}/ZONES/${vh.zone}/VEHICLES/${vh.id}/TRAILS`;
+    data = {
+      ...data,
+      sent_tsmp: moment(data.sent_tsmp).format()
+    }
+    fB.child(path).push().set(data).then(function (result) {
       return res.json({ message: 'Data submitted succesfully' })
     })
       .catch(function (e) {
@@ -42,23 +42,31 @@ router.post('/savetrail', (req, res) => {
 })
 
 router.post('/downloadcsv', (req, res) => {
+  const {
+    vehicle,
+    startingDate,
+    endingDate
+  } = req.body;
+  const company = vehicle.zone.slice(0, 5);
+  const path = `DATA/ENTITIES/${company}/ZONES/${vehicle.zone}/VEHICLES/${vehicle.id}/TRAILS`;
+
   const time1 = moment(new Date());
   // console.log('*** time 1 -> ', time1.format('DD/MM HH:mm:ss'));
-  fB.child('trails').orderByChild('sent_tsmp').startAt(req.body.startingDate).endAt(req.body.endingDate)
+  fB.child(path).orderByChild('sent_tsmp').startAt(startingDate).endAt(endingDate)
     .on('value', snap => {
       const snapVal = snap.val();
-      const time2 = moment(new Date());
+      // const time2 = moment(new Date());
       // console.log('*** time 2 -> ', time2.format('DD/MM HH:mm:ss'), '.:. Diff ->', moment(time2.diff(time1)).format("m[m] s[s]"));
       if (!snapVal) return res.status(400).send({ message: 'Los resultados no contienen registros. Modifique los parámetros de búsqueda.' });
       const data = Object.values(snap.val())
-      const fields = ['id', 'altitude', 'latitude', 'longitude', 'sent_tsmp', 'speed', 'track', 'vehicle_id', 'zone_vehicle'];
+      const fields = ['id', 'altitude', 'lat', 'lng', 'sent_tsmp', 'speed', 'track', 'vehicle_id', 'zone_vehicle'];
       const opts = { fields };
-      const time3 = moment(new Date());
+      // const time3 = moment(new Date());
       // console.log('*** time 3 -> ', time3.format('DD/MM HH:mm:ss'), '.:. Diff ->', moment(time3.diff(time2)).format("m[m] s[s]"));
       const csv = json2csv(data, opts);
-      const time4 = moment(new Date());
+      // const time4 = moment(new Date());
       // console.log('*** time 4 -> ', time3.format('DD/MM HH:mm:ss'), '.:. Diff ->', moment(time4.diff(time3)).format("m[m] s[s]"));
-      
+
       return res.status(200).send(csv);
     })
 })
