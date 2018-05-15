20d54f99962d905bff100d0679fe2876e1e7886d
Jeison Berdugo <jeison@vstrokes.com>
2018-05-15 07:40:25 -0500

    Implemented detecting entering geo-fence

--- api.js
+++ api.js
@@ -4,10 +4,11 @@ const router = express.Router();
 const json2csv = require('json2csv').parse;
 const firebase = require("firebase-admin");
 const serviceAccount = require('./firebase.json');
-const fs = require('fs');
+// const fs = require('fs');
 const moment = require('moment');
+const geodistance = require('./geodistance');
 
-const dbURL = "https://ss-smtracking.firebaseio.com"
+const dbURL = "https://ss-smtracking.firebaseio.com";
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount),
   databaseURL: dbURL
@@ -19,27 +20,42 @@ exports.getCompanyOfVehicle = (vehicle_id, done) => {
   fB.child('CONTROL/VEHICLES').orderByChild('id').equalTo(vehicle_id).on('value', snap => {
     done(snap.val());
   })
-}
+};
 
 router.post('/savetrail', (req, res) => {
   let data = req.body;
-  exports.getCompanyOfVehicle(data.vehicle_id, cb => {
-    if (!cb) return res.status(404).send('Vehicle not found.')
+  exports.getCompanyOfVehicle(data.vehicle_id, async cb => {
+    if (!cb) return res.status(404).send('Vehicle not found.');
     const vh = cb[Object.keys(cb)[0]];
     const company = vh.zone.slice(0, 5);
     const path = `DATA/ENTITIES/${company}/ZONES/${vh.zone}/VEHICLES/${vh.id}/TRAILS`;
     data = {
       ...data,
       sent_tsmp: moment(data.sent_tsmp).utcOffset("-05:00").format()
+    };
+
+    try {
+      const lastSnap = await fB.child(path).limitToLast(1).once('value');
+      const last = Object.values(lastSnap.val())[0];
+      await fB.child(path).push().set(data);
+      const geoFencesSnap = fB.child(`CONTROL/GEO_FENCES/${company}`).once('value');
+      const geoFences = geoFencesSnap.val();
+      Object.values(geoFences).forEach(async geoFence => {
+        const lastDistance = geodistance(geoFence.lat, geoFence.lng, last.lat, last.lng);
+        const recentDistance = geodistance(geoFence.lat, geoFence.lng, data.lat, data.lng);
+        if (lastDistance > geoFence.radius && recentDistance < geoFence.radius) { // vehicle enters geo-fence
+          // Get users
+          const usersSnap = await fB.child('USERS').orderByChild('company').equalTo(company).once('value');
+          const users = Object.values(usersSnap.val()); // { company, email, name, role }
+          // TODO send emails
+        }
+        // if (lastDistance < geoFence.radius && recentDistance > geoFence.radius) // vehicle leaves geo-fence
+      });
+    } catch (e) {
+      return res.status(500).send(e.message);
     }
-    fB.child(path).push().set(data).then(function (result) {
-      return res.json({ message: 'Data submitted succesfully' })
-    })
-      .catch(function (e) {
-        return res.status(500).send(e.message);
-      })
-  })
-})
+  });
+});
 
 router.post('/downloadcsv', (req, res) => {
   const {
@@ -50,7 +66,7 @@ router.post('/downloadcsv', (req, res) => {
   const company = vehicle.zone.slice(0, 5);
   const path = `DATA/ENTITIES/${company}/ZONES/${vehicle.zone}/VEHICLES/${vehicle.id}/TRAILS`;
 
-  const time1 = moment(new Date());
+  // const time1 = moment(new Date());
   // console.log('*** time 1 -> ', time1.format('DD/MM HH:mm:ss'));
   fB.child(path).orderByChild('sent_tsmp').startAt(startingDate).endAt(endingDate)
     .on('value', snap => {
@@ -58,7 +74,7 @@ router.post('/downloadcsv', (req, res) => {
       // const time2 = moment(new Date());
       // console.log('*** time 2 -> ', time2.format('DD/MM HH:mm:ss'), '.:. Diff ->', moment(time2.diff(time1)).format("m[m] s[s]"));
       if (!snapVal) return res.status(400).send({ message: 'Los resultados no contienen registros. Modifique los parámetros de búsqueda.' });
-      const data = Object.values(snap.val())
+      const data = Object.values(snap.val());
       const fields = ['id', 'altitude', 'lat', 'lng', 'sent_tsmp', 'speed', 'track', 'vehicle_id', 'zone_vehicle'];
       const opts = { fields };
       // const time3 = moment(new Date());
@@ -68,7 +84,7 @@ router.post('/downloadcsv', (req, res) => {
       // console.log('*** time 4 -> ', time3.format('DD/MM HH:mm:ss'), '.:. Diff ->', moment(time4.diff(time3)).format("m[m] s[s]"));
 
       return res.status(200).send(csv);
-    })
-})
+    });
+});
 
 module.exports = router;
---
+++ geodistance.js
@@ -0,0 +1,19 @@
+const calcArc = angle => angle * Math.PI / 180;
+const calcSin2 = x => Math.pow(Math.sin(x), 2);
+
+module.exports = (lat1, lng1, lat2, lng2) => {
+  if (!lat1 || !lng1 || !lat2 || !lng2) {
+    return null;
+  }
+  const radius = 6371e3; // Earth radius
+  const phi1 = calcArc(lat1);
+  const lambda1 = calcArc(lng1);
+  const phi2 = calcArc(lat1);
+  const lambda2 = calcArc(lng2);
+  const deltaPhi = phi2 - phi1;
+  const deltaLambda = lambda2 - lambda1;
+
+  const a = calcSin2(deltaPhi/2) + Math.cos(phi1) * Math.cos(phi2) * calcSin2(deltaLambda/2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return radius * c;
+};
